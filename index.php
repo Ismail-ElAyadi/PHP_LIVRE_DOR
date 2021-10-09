d864d80d9f726521a385ca842b705f19aacc96fc
Rifi Ismail <ismail.elydi@gmail.com>
2021-10-09 15:55:12 +0200

    init

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$title = "Livre d'or" ;
+require 'elements/header.php';
+?>
+<div class="container">
+    <h1>Livre d'or</h1>
+    <form action="" method="post">
+        <div class="form-group">
+            <input type="text" class="form-control"name="username" placeholder="Votre pseudo" id="">  
+        </div>
+        <div class="form-group">
+            <textarea name="message" id="" cols="30" rows="10"></textarea>
+        </div>
+        <button class="btn btn-primary">Envoyer</button>
+    </form>
+</div>
+
+<?php 
+require 'elements/footer.php';
+?>
